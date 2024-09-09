37b5445611ab41185d79010258273bd7ed1cc975
Ashish Yadav <red458718@gmail.com>
2024-09-10 04:23:48 +0530

    Deploying Changes

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from 'path'
 import authRoutes from './routes/auth.routes.js'
 import messageRoute from './routes/message.route.js'
 import userRoute from './routes/user.routes.js';
@@ -8,7 +9,7 @@ import cookieParser from "cookie-parser";
 import {server,io,app} from './Socket/socket.js'
 const PORT = process.env.PORT || 5000;
 dotenv.config();
-
+const __dirname = path.resolve();
 // app.get('/', (req, res)=>{
 //     res.send('Hello from Backend');
 // })
@@ -17,6 +18,11 @@ app.use(express.json()); // to parse incoming requests from frontend to json pay
 app.use('/api/auth', authRoutes)   // Middleware to perfrom authentication. 
 app.use('/api/user', userRoute)
 app.use('/api/messages',messageRoute)
+app.use(express.static(path.join(__dirname,"/Frontend/dist")));
+app.get('*',(req,res)=>{
+   res.sendFile(path.join(__dirname,"/Frontend","dist","index.html"))
+})
+   
 server.listen(PORT, ()=>{
    connectToMongoDB();
    console.log(`Server is running at PORT ${PORT}`);
